5064b5e8d2437f455880e39e7ebba425a9238f36
Tomi <halmos.tamas001@gmail.com>
2025-07-26 16:13:08 +0200

    simple jump&groundcheck

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-
+    [SerializeField] private float jumpForce = 5f;
+    CapsuleCollider2D playerCollider;
+    public LayerMask whatIsGround;
     private Rigidbody2D myRigidbody;
 
     Animator animator ;
@@ -12,23 +14,46 @@ public class PlayerMovement : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         Movement();
         FlipSprite();
+        Jump();
     }
 
+    private bool isGrounded()
+    {
+        float extraHeight = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast
+        (
+            playerCollider.bounds.center,
+            playerCollider.bounds.size,
+            0f,
+            UnityEngine.Vector2.down,
+            extraHeight,
+            whatIsGround
+        );
+        return raycastHit.collider != null;
+     }
 
     void Movement()
     {
         float moveInput = Input.GetAxis("Horizontal");
         myRigidbody.linearVelocity = new UnityEngine.Vector2(moveInput * moveSpeed, myRigidbody.linearVelocity.y);
         animator.SetFloat("Velocity", Mathf.Abs(myRigidbody.linearVelocityX));
-
-
     }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            myRigidbody.linearVelocity = new UnityEngine.Vector2(myRigidbody.linearVelocity.x,jumpForce);
+        }
+
+     }
     void FlipSprite()
     {
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.linearVelocityX) > Mathf.Epsilon;
